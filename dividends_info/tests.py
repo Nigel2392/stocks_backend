56ca93d149669dbed03cd3f4ec4c7b96e3612d51
Cody Childers <cchilder@mail.usf.edu>
2022-07-26 16:55:19 -0400

    idk

--- dividends_info/tests.py
+++ dividends_info/tests.py
@@ -4,8 +4,11 @@ from django.test import TestCase
 
 from .data import DIVIDENDS_DATA_FOR_PSEC
 
+from .functions import (
+    get_dividends_within_time_span,
+)
 
-def dividends_data_to_datetime(data):
+def dividends_data_string_date_to_datetime(data):
     datetime_data = []
     for dict in data:
         datetime_data.append({'date': datetime.datetime.strptime(dict['date'], "%m/%d/%Y"), 'amount': dict['amount']})
@@ -13,10 +16,19 @@ def dividends_data_to_datetime(data):
 
 class DividendsFunctionsTests(TestCase):
 
-    def test_this(self):
-        data = dividends_data_to_datetime(DIVIDENDS_DATA_FOR_PSEC)
-        print(data)
-        # https://github.com/martinrusev/django-quick-test/issues/4
-        # sys.stderr.write(repr(data) + '\n')
-        # import ipdb; ipdb.set_trace()
-        self.assertIs(True, False)
+    def test_get_dividends_within_time_span_1(self):
+        dividends_data = dividends_data_string_date_to_datetime(DIVIDENDS_DATA_FOR_PSEC)
+        actual_dividends = get_dividends_within_time_span(dividends_data, datetime.datetime(2021, 7, 25), datetime.datetime(2022, 7, 25))
+        expected = [{'date': datetime.datetime(2021, 7, 27, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2021, 8, 26, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2021, 9, 27, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2021, 10, 26, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2021, 11, 24, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2021, 12, 28, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2022, 1, 26, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2022, 2, 23, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2022, 3, 28, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2022, 4, 26, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2022, 5, 26, 0, 0), 'amount': 0.06},
+                    {'date': datetime.datetime(2022, 6, 27, 0, 0), 'amount': 0.12}]
+        self.assertEqual(actual_dividends, expected)
