fdb92cb5164c6e3417fa4a7db79cd332d1aef60d
Cody <cchilder@mail.usf.edu>
2022-08-02 21:48:33 -0400

    fixed dividends endpoint

--- dividends_info/urls.py
+++ dividends_info/urls.py
@@ -6,11 +6,6 @@ app_name = 'dividends'
 urlpatterns = [
     path('<str:ticker>', views.main_dividends_results, name='dividends_data'),
 
-    # path('current_price/<str:ticker>', views.current_price_view, name='current_price'),
-    # path('dividend_yield_change/<str:ticker>/<int:years_ago>/', views.get_dividend_yield_change_for_certain_years_ago_view, name='dividend_change'),
-    # path('current_yield/<str:ticker>/', views.current_dividend_yield_view, name='current_yield'),
-    # path('all_dividends/<str:ticker>/<int:years_back>/', views.dividends_over_last_certain_years, name='all_dividends_over_time'),
-    # path('recent_dividend_rate/<str:ticker>/', views.recent_yearly_dividend_rate, name='recent_dividend_rate'),
-
+    path('all_dividends/<str:ticker>/<int:years_back>/', views.dividends_over_last_certain_years, name='all_dividends_over_time'),
 
 ]
--- dividends_info/views.py
+++ dividends_info/views.py
@@ -49,37 +49,19 @@ def main_dividends_results(request, ticker):
     current_yield = get_current_dividend_yield(current_price, dividends)
     dividends_data['current_yield'] = current_yield
 
+    rate = get_yearly_dividend_rate_from_date(dividends, today)
+    dividends_data['recent_dividend_rate'] = rate
+
+    # get dividends
+    YEARS_BACK = 3
+    days_ago = years_back * 365
+    years_back_datetime = today - datetime.timedelta(days=days_ago)
+    dividends_over_certain_year_timespan = get_dividends_within_time_span(dividends, years_back_datetime, today)
+    formatted_dividends_data = dividends_datetime_to_string(dividends_over_certain_year_timespan)
+    dividends_data['all_dividends'] = formatted_dividends_data
+
     json_data = json.dumps(dividends_data)
     return HttpResponse(json_data, content_type='application/json')
-
-
-# def current_price_view(request, ticker):
-#     yahoo_stock_obj = yfinance.Ticker(ticker.upper())
-#     current_price = get_current_price(yahoo_stock_obj)
-#     current_price_object = {'current_price': current_price}
-#     data = json.dumps(current_price_object)
-#     return HttpResponse(data, content_type='application/json')
-
-
-
-# def get_dividend_yield_change_for_certain_years_ago_view(request, ticker, years_ago):
-#     dividends = get_dividends(ticker)
-#     today = datetime.date.today()
-#     change = get_dividend_change_over_years(dividends, years_ago, today)
-#     change_object = {'change': change}
-#     data = json.dumps(change_object)
-#     return HttpResponse(data, content_type='application/json')
-
-
-def current_dividend_yield_view(request, ticker):
-    yahoo_stock_obj = yfinance.Ticker(ticker.upper())
-    # import ipdb; ipdb.set_trace()
-    price = get_current_price(yahoo_stock_obj)
-    dividends = get_all_dividends(yahoo_stock_obj)
-    current_yield = get_current_dividend_yield(price, dividends)
-    current_yield_object = {'current_yield': current_yield}
-    data = json.dumps(current_yield_object)
-    return HttpResponse(data, content_type='application/json')
 
 
 def dividends_over_last_certain_years(request, ticker, years_back):
@@ -90,17 +72,4 @@ def dividends_over_last_certain_years(request, ticker, years_back):
     dividends_over_certain_year_timespan = get_dividends_within_time_span(dividends, years_back_datetime, today)
     formatted_data = dividends_datetime_to_string(dividends_over_certain_year_timespan)
     data = json.dumps(formatted_data)
-    return HttpResponse(data, content_type='application/json')
-
-
-def recent_yearly_dividend_rate(request, ticker):
-    yahoo_stock_obj = yfinance.Ticker(ticker.upper())
-    dividends = get_all_dividends_dicts(yahoo_stock_obj)
-    print("dividends")
-    print(dividends)
-    # import ipdb; ipdb.set_trace()
-    today = datetime.date.today()
-    rate = get_yearly_dividend_rate_from_date(dividends, today)
-    rate_object = {'year_dividend_rate': rate}
-    data = json.dumps(rate_object)
     return HttpResponse(data, content_type='application/json')
