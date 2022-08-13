b6db9b064a169a62a8e3591aae696b479fab5308
Cody <cchilder@mail.usf.edu>
2022-08-13 12:14:38 -0400

    working on post request for users

--- users/views.py
+++ users/views.py
@@ -7,21 +7,53 @@ from .models import UserProfile
 
 
 def get_user_profile(request, user_id):
-    try:
-        user = UserProfile.objects.get(user_id=user_id)
-        print("user found")
-    except:
-        user = None
-        profile = UserProfile()
-        profile.user_id = user_id
-        profile.searches = []
-        profile.save()
-        print("user saved in db")
-        user = UserProfile.objects.get(user_id=user_id)
+    if request.method == 'GET':
+        try:
+            user = UserProfile.objects.get(user_id=user_id)
+            print("user found")
+        except Exception as error:
+            print("User profile wasnt found:")
+            print(error)
+            user = None
+            profile = UserProfile()
+            profile.user_id = user_id
+            profile.searches = [
+                {'search_term': 'hd'},
+                {'search_term': 'wba'},
+            ]
+            profile.save()
+            print("user saved in db")
+            user = UserProfile.objects.get(user_id=user_id)
 
-    data = {
-        'user_id': user.user_id,
-        'searches': user.searches
-    }
-    json_data = json.dumps(data)
-    return HttpResponse(json_data, content_type='application/json')
+        data = {
+            'user_id': user.user_id,
+            'searches': user.searches
+        }
+        json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type='application/json')
+    if request.method == 'POST':
+        pass
+
+
+# def update_user_profile(request, user_id):
+#     try:
+#         user = UserProfile.objects.get(user_id=user_id)
+#         print("user found")
+#     except:
+#         user = None
+#         profile = UserProfile()
+#         profile.user_id = user_id
+#         profile.searches = [
+#             {'search_term': 'hd'},
+#             {'search_term': 'wba'},
+#         ]
+#         profile.save()
+#         print("user saved in db")
+#         user = UserProfile.objects.get(user_id=user_id)
+#
+#     data = {
+#         'user_id': user.user_id,
+#         'searches': user.searches
+#     }
+#     json_data = json.dumps(data)
+#     return HttpResponse(json_data, content_type='application/json')
