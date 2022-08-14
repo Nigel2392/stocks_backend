1b55c767a148b917d9dff60a70b403cac2529b7e
Cody <cchilder@mail.usf.edu>
2022-08-14 12:10:10 -0400

    remove old code

--- users/views.py
+++ users/views.py
@@ -37,11 +37,6 @@ def get_user_profile(request, user_id):
         return HttpResponse({json_data}, content_type='application/json')
 
     if request.method == 'POST':
-        # raw_data = request.body
-        # """https://stackoverflow.com/questions/29780060/trying-to-parse-request-body-from-post-in-django"""
-        # body_unicode = raw_data.decode('utf-8')
-        # body = json.loads(body_unicode)
-        # print(body)
         body = parse_request_body(request)
         searches = body['searches']
         searches_objects = [{'search_term': x} for x in searches]
